2a575c9371eeb8dbdfc2a7c040d5384246290e4f
Adam Stacey <adamconradstacey@gmail.com>
2020-09-22 15:08:04 +1000

    Switched to nodert packages

--- install.js
+++
@@ -1,5 +0,0 @@
-if (require('os').platform() === 'win32') {
-    var installProcess = require('child_process').exec('install.cmd');
-    installProcess.stdout.pipe(process.stdout);
-    installProcess.stderr.pipe(process.stderr);
-}
