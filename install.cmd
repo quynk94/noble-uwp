e1111042f03b1cdb2419c5b6e75c4e1f1d65fded
Adam Stacey <adamconradstacey@gmail.com>
2020-09-24 17:56:46 +1000

    Move to optionalDependencies but force check on windows

--- install.cmd
+++
@@ -1,9 +0,0 @@
-call npm install --no-save ^
-@nodert-win10-cu/windows.devices.bluetooth.genericattributeprofile@github:acstacey/nodert-win10-cu-windows.devices.bluetooth.genericattributeprofile ^
-@nodert-win10-cu/windows.devices.bluetooth ^
-@nodert-win10-cu/windows.devices.bluetooth.advertisement ^
-@nodert-win10-cu/windows.devices.enumeration ^
-@nodert-win10-cu/windows.devices.radios ^
-@nodert-win10-cu/windows.foundation ^
-@nodert-win10-cu/windows.storage.streams
-if %errorlevel% neq 0 exit %errorlevel%
