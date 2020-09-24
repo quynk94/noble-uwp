b44b52411245f51eb7600f1a16e2e9a3df4c0cc6
Adam Stacey <adamconradstacey@gmail.com>
2020-09-24 16:30:03 +1000

    Added windows specific install script

---
+++ install.cmd
@@ -0,0 +1,9 @@
+call npm install --no-save ^
+@nodert-win10-cu/windows.devices.bluetooth.genericattributeprofile@github:acstacey/nodert-win10-cu-windows.devices.bluetooth.genericattributeprofile ^
+@nodert-win10-cu/windows.devices.bluetooth ^
+@nodert-win10-cu/windows.devices.bluetooth.advertisement ^
+@nodert-win10-cu/windows.devices.enumeration ^
+@nodert-win10-cu/windows.devices.radios ^
+@nodert-win10-cu/windows.foundation ^
+@nodert-win10-cu/windows.storage.streams
+if %errorlevel% neq 0 exit %errorlevel%
